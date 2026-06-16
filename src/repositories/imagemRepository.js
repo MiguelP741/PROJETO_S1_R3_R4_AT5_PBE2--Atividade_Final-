1c4d6fcc227e6f0013d0298faebae3b2c68c9cc4
MIGUEL PEREIRA ARAUJO <miguel.araujo25@senaisp.edu.br>
2026-06-16 16:13:18 -0300

    Correção imagem 3

--- src/repositories/imagemRepository.js
+++ src/repositories/imagemRepository.js
@@ -2,7 +2,7 @@ import { connection } from "../configs/Database.js";
 
 const imagemRepository = {
   listar: async (id) => {
-    const sql = "SELECT Imagem FROM Produtos WHERE id = ?";
+    const sql = "SELECT Imagem FROM Produtos WHERE IdProduto = ?";
     const values = [id];
     const [rows] = await connection.execute(sql, values);
     return rows; 
